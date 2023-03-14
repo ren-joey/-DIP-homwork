b029843b22fed25a2dabde399b1b0d30ec6b6972
Joey_Ren <tsuyiren@gmail.com>
2023-03-14 18:19:29 +0800

    feat: 🎸 generate images for report

--- hw1_p11922004/hw1.py
+++ hw1_p11922004/hw1.py
@@ -2,23 +2,32 @@ import math
 
 import cv2
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 from utilities import \
                     plot_histograms,\
                     plot_histogram, \
                     plot_ghe, \
                     low_pass_filter, \
-                    median_filter
+                    median_filter, \
+                    get_psnr
 
 
 # ============================================================
 # ==================== Problem 0: Warn Up ====================
 # ============================================================
 
-img = cv2.imread('./hw1_sample_images/sample1.png')
-img = np.array(img)
+oimg = cv2.imread('./hw1_sample_images/sample1.png')
+oimg = cv2.cvtColor(oimg, cv2.COLOR_BGR2RGB)
+oimg = np.array(oimg)
 
 # (a) convert sample1.png to a gray-scaled image named result1.png.
-# formula: 0.2989 * R + 0.5870 * G + 0.1140 * B
+# Grayscale transformation formula applied
+# lightness: 			        	    (max(R, G, B) + min(R, G, B)) / 2.
+# average: 			                	(R + G + B) / 3
+# person’s perception of brightness:  	0.2989 * R + 0.5870 * G + 0.1140 * B
+# luminosity: 			            	0.21 R + 0.72 G + 0.07 B
+img = np.array(oimg)
 for row in img:
     for (idx, pixel) in enumerate(row):
         row[idx] = np.sum(
@@ -26,11 +35,67 @@ for row in img:
         )
 cv2.imwrite('./result1.png', img)
 
+img2 = np.array(oimg)
+for row in img2:
+    for (idx, pixel) in enumerate(row):
+        row[idx] = np.sum(
+            pixel * [0.21, 0.72, 0.07]
+        )
+
+img3 = np.array(oimg)
+for row in img3:
+    for (idx, pixel) in enumerate(row):
+        row[idx] = np.sum(pixel) / 3
+
+img4 = np.array(oimg)
+for row in img4:
+    for (idx, pixel) in enumerate(row):
+        max = float(np.max(pixel))
+        min = float(np.min(pixel))
+        row[idx] = int(math.ceil((max + min) / 2))
+
+matplotlib.rc('font', **{'size': 6})
+plt.figure(dpi=300)
+plt.subplot(1, 5, 1)
+plt.title('origin')
+plt.imshow(oimg)
+plt.axis('off')
+plt.subplot(1, 5, 2)
+plt.title('lightness')
+plt.imshow(img4)
+plt.axis('off')
+plt.subplot(1, 5, 3)
+plt.title('average')
+plt.imshow(img3)
+plt.axis('off')
+plt.subplot(1, 5, 4)
+plt.title('person’s perception')
+plt.imshow(img)
+plt.axis('off')
+plt.subplot(1, 5, 5)
+plt.title('luminosity')
+plt.imshow(img2)
+plt.axis('off')
+plt.show()
+plt.close()
+
 # (b) Perform vertical flipping on result1.png and output the result as result2.png
 height = img.shape[0]
-for i in range(math.floor(height / 2)):
-    img[i], img[-i] = np.copy(img[-i]), np.copy(img[i])
-cv2.imwrite('./result2.png', img)
+G = np.full(img.shape, 0)
+for i in range(math.ceil(height / 2) + 1):
+    G[i], G[-i] = img[-i], img[i]
+cv2.imwrite('./result2.png', G)
+
+plt.subplot(1, 2, 1)
+plt.title('origin')
+plt.imshow(img)
+plt.axis('off')
+plt.subplot(1, 2, 2)
+plt.title('vertical flipping')
+plt.imshow(G)
+plt.axis('off')
+plt.show()
+plt.close()
 
 # ============================================================
 # =============== Problem 1: IMAGE ENHANCEMENT ===============
@@ -60,7 +125,7 @@ cv2.imwrite('./result4.png', img2b)
 # What can you observe from these three histograms?
 plot_histograms(
     imgs=[img2, img2a, img2b],
-    titles=['histogram2', 'histogram2a', 'histogram2b'],
+    titles=['original histogram', 'P/3', 'P/3*3'],
     filename='histograms.png'
 )
 
@@ -83,7 +148,7 @@ cv2.imwrite('./result7.png', G7)
 
 plot_histograms(
     imgs=[G5, G6, G7],
-    titles=['result5_histogram', 'result6_histogram', 'result7_histogram'],
+    titles=['original GHE', 'P/3 GHE', 'P/3*3 GHE'],
     filename='GHE_histograms.png'
 )
 
@@ -119,6 +184,10 @@ cv2.imwrite('./result12.png', result12)
 
 result13 = median_filter(img5, filtersize=5, crossmode=True)
 cv2.imwrite('./result13.png', result13)
+
+psnr12 = get_psnr(img3, result12)
+psnr13 = get_psnr(img3, result13)
+
 
 # (b) (10 pt) In noise removal problems,
 # PSNR is a widely used metric to present the quality of your recovered image.
